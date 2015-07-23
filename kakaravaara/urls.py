a68cf5b9c655ddea5001bbf36987ca6c524ba87c
Jason Robinson <mail@jasonrobinson.me>
2015-07-24 00:02:50 +0300

    Add language changer to navigation

--- kakaravaara/urls.py
+++ kakaravaara/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('reservations.urls', namespace="reservations")),
     url(r'^sa/', include('shoop.admin.urls', namespace="shoop_admin", app_name="shoop_admin")),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^', include('shoop.front.urls', namespace="shoop", app_name="shoop")),
 ]
