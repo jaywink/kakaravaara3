dcea459dfd63242d28976be35180d5855f447ce6
Jason Robinson <mail@jasonrobinson.me>
2016-06-12 21:40:18 +0300

    Upstream has fixed bug with force_new_line
    Removing override that was in place due to shuup/shuup#291

--- reservations/basket.py
+++ reservations/basket.py
@@ -8,24 +8,15 @@ from reservations.models import Reservation, ReservableProduct
 
 
 class ReservableBasket(BaseBasket):
-    def _compare_line_for_addition(self, current_line_data, product, supplier, shop, extra):
-        if product.type.identifier == "reservable":
-            # Never add to existing reservation lines
-            return False
-        return super(
-            ReservableBasket, self)._compare_line_for_addition(current_line_data, product, supplier, shop, extra)
-
     def add_product(self, supplier, shop, product, quantity, force_new_line=False, extra=None, parent_line=None):
         if not extra:
             extra = {}
         if self.request.POST.get("start", None):
             extra["reservation_start"] = parse_date(self.request.POST.get("start"))
             extra["persons"] = self.request.POST.get("persons", 1)
-        # TODO: enable this here once https://github.com/shoopio/shoop/issues/291 is resolved in some way
-        # Currently setting `force_new_line` causes product not to be added at all.
-        # Once this works, remove above override of `_compare_line_for_addition`.
-        # if product.type.identifier == "reservable":
-        #     force_new_line = True
+        # Always add a new line for reservations
+        if product.type.identifier == "reservable":
+            force_new_line = True
         return super(ReservableBasket, self).add_product(
             supplier, shop, product, quantity, force_new_line=force_new_line, extra=extra, parent_line=parent_line)
 
